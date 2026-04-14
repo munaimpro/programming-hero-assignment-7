3c363974baefdf7b436591b586d1e9102d7b3b13
Munaim Khan <khanmail2599@gmail.com>
2026-04-14 11:43:30 +0600

    feat: friend list component design

---
+++ keen-keeper/src/components/HomePage/Friends/Friends.jsx
@@ -0,0 +1,143 @@
+import React from 'react';
+import friend1 from '../../../assets/Ellipse1.png'
+
+const Friends = () => {
+    return (
+        <div className='container mx-auto'>
+            <h2 className='text-[#1F2937] font-semibold text-2xl mb-4'>Your friends</h2>
+            <div className="grid gap-6 grid-col-1 lg:grid-cols-4">
+                {/* Friend 1 */}
+                <div className='card bg-base-100 shadow text-center rounded-xl p-8 justify-center items-center'>
+                    <div className='bg-[#D9D9D9] rounded-full mb-3'>
+                        <img src={friend1} alt="Friend" />
+                    </div>
+                    <h2 className='text-[20px] font-semibold mb-2 text-[#1F2937]'>David Kim</h2>
+                    <p className='text-[#64748B] text-[12px] mb-2'>62d ago</p>
+                    <div className="badge badge-success bg-[#CBFADB] border-[#CBFADB] text-[#244D3F] rounded-2xl font-medium text-[12px] uppercase py-1.5 px-2 mb-2">Work</div>
+                    <div className="badge badge-success bg-[#EFAD44] border-[#EFAD44] text-base-100 rounded-2xl font-medium text-[12px] capitalize py-1.5 px-2 mb-2">Almost Due</div>
+                    <div className="badge badge-success bg-[#EF4444] border-[#EF4444] text-base-100 rounded-2xl font-medium text-[12px] capitalize py-1.5 px-2 mb-2">Overdue</div>
+                    <div className="badge badge-success bg-[#244D3F] border-[#244D3F] text-base-100 rounded-2xl font-medium text-[12px] capitalize py-1.5 px-2 mb-2">On-Track</div>
+                </div>
+
+                {/* Friend 1 */}
+                <div className='card bg-base-100 shadow text-center rounded-xl p-8 justify-center items-center'>
+                    <div className='bg-[#D9D9D9] rounded-full mb-3'>
+                        <img src={friend1} alt="Friend" />
+                    </div>
+                    <h2 className='text-[20px] font-semibold mb-2 text-[#1F2937]'>David Kim</h2>
+                    <p className='text-[#64748B] text-[12px] mb-2'>62d ago</p>
+                    <div className="badge badge-success bg-[#CBFADB] border-[#CBFADB] text-[#244D3F] rounded-2xl font-medium text-[12px] uppercase py-1.5 px-2 mb-2">Work</div>
+                    <div className="badge badge-success bg-[#EFAD44] border-[#EFAD44] text-base-100 rounded-2xl font-medium text-[12px] capitalize py-1.5 px-2 mb-2">Almost Due</div>
+                    <div className="badge badge-success bg-[#EF4444] border-[#EF4444] text-base-100 rounded-2xl font-medium text-[12px] capitalize py-1.5 px-2 mb-2">Overdue</div>
+                    <div className="badge badge-success bg-[#244D3F] border-[#244D3F] text-base-100 rounded-2xl font-medium text-[12px] capitalize py-1.5 px-2 mb-2">On-Track</div>
+                </div>
+
+                {/* Friend 1 */}
+                <div className='card bg-base-100 shadow text-center rounded-xl p-8 justify-center items-center'>
+                    <div className='bg-[#D9D9D9] rounded-full mb-3'>
+                        <img src={friend1} alt="Friend" />
+                    </div>
+                    <h2 className='text-[20px] font-semibold mb-2 text-[#1F2937]'>David Kim</h2>
+                    <p className='text-[#64748B] text-[12px] mb-2'>62d ago</p>
+                    <div className="badge badge-success bg-[#CBFADB] border-[#CBFADB] text-[#244D3F] rounded-2xl font-medium text-[12px] uppercase py-1.5 px-2 mb-2">Work</div>
+                    <div className="badge badge-success bg-[#EFAD44] border-[#EFAD44] text-base-100 rounded-2xl font-medium text-[12px] capitalize py-1.5 px-2 mb-2">Almost Due</div>
+                    <div className="badge badge-success bg-[#EF4444] border-[#EF4444] text-base-100 rounded-2xl font-medium text-[12px] capitalize py-1.5 px-2 mb-2">Overdue</div>
+                    <div className="badge badge-success bg-[#244D3F] border-[#244D3F] text-base-100 rounded-2xl font-medium text-[12px] capitalize py-1.5 px-2 mb-2">On-Track</div>
+                </div>
+
+                {/* Friend 1 */}
+                <div className='card bg-base-100 shadow text-center rounded-xl p-8 justify-center items-center'>
+                    <div className='bg-[#D9D9D9] rounded-full mb-3'>
+                        <img src={friend1} alt="Friend" />
+                    </div>
+                    <h2 className='text-[20px] font-semibold mb-2 text-[#1F2937]'>David Kim</h2>
+                    <p className='text-[#64748B] text-[12px] mb-2'>62d ago</p>
+                    <div className="badge badge-success bg-[#CBFADB] border-[#CBFADB] text-[#244D3F] rounded-2xl font-medium text-[12px] uppercase py-1.5 px-2 mb-2">Work</div>
+                    <div className="badge badge-success bg-[#EFAD44] border-[#EFAD44] text-base-100 rounded-2xl font-medium text-[12px] capitalize py-1.5 px-2 mb-2">Almost Due</div>
+                    <div className="badge badge-success bg-[#EF4444] border-[#EF4444] text-base-100 rounded-2xl font-medium text-[12px] capitalize py-1.5 px-2 mb-2">Overdue</div>
+                    <div className="badge badge-success bg-[#244D3F] border-[#244D3F] text-base-100 rounded-2xl font-medium text-[12px] capitalize py-1.5 px-2 mb-2">On-Track</div>
+                </div>
+
+                {/* Friend 1 */}
+                <div className='card bg-base-100 shadow text-center rounded-xl p-8 justify-center items-center'>
+                    <div className='bg-[#D9D9D9] rounded-full mb-3'>
+                        <img src={friend1} alt="Friend" />
+                    </div>
+                    <h2 className='text-[20px] font-semibold mb-2 text-[#1F2937]'>David Kim</h2>
+                    <p className='text-[#64748B] text-[12px] mb-2'>62d ago</p>
+                    <div className="badge badge-success bg-[#CBFADB] border-[#CBFADB] text-[#244D3F] rounded-2xl font-medium text-[12px] uppercase py-1.5 px-2 mb-2">Work</div>
+                    <div className="badge badge-success bg-[#EFAD44] border-[#EFAD44] text-base-100 rounded-2xl font-medium text-[12px] capitalize py-1.5 px-2 mb-2">Almost Due</div>
+                    <div className="badge badge-success bg-[#EF4444] border-[#EF4444] text-base-100 rounded-2xl font-medium text-[12px] capitalize py-1.5 px-2 mb-2">Overdue</div>
+                    <div className="badge badge-success bg-[#244D3F] border-[#244D3F] text-base-100 rounded-2xl font-medium text-[12px] capitalize py-1.5 px-2 mb-2">On-Track</div>
+                </div>
+
+                {/* Friend 1 */}
+                <div className='card bg-base-100 shadow text-center rounded-xl p-8 justify-center items-center'>
+                    <div className='bg-[#D9D9D9] rounded-full mb-3'>
+                        <img src={friend1} alt="Friend" />
+                    </div>
+                    <h2 className='text-[20px] font-semibold mb-2 text-[#1F2937]'>David Kim</h2>
+                    <p className='text-[#64748B] text-[12px] mb-2'>62d ago</p>
+                    <div className="badge badge-success bg-[#CBFADB] border-[#CBFADB] text-[#244D3F] rounded-2xl font-medium text-[12px] uppercase py-1.5 px-2 mb-2">Work</div>
+                    <div className="badge badge-success bg-[#EFAD44] border-[#EFAD44] text-base-100 rounded-2xl font-medium text-[12px] capitalize py-1.5 px-2 mb-2">Almost Due</div>
+                    <div className="badge badge-success bg-[#EF4444] border-[#EF4444] text-base-100 rounded-2xl font-medium text-[12px] capitalize py-1.5 px-2 mb-2">Overdue</div>
+                    <div className="badge badge-success bg-[#244D3F] border-[#244D3F] text-base-100 rounded-2xl font-medium text-[12px] capitalize py-1.5 px-2 mb-2">On-Track</div>
+                </div>
+
+                {/* Friend 1 */}
+                <div className='card bg-base-100 shadow text-center rounded-xl p-8 justify-center items-center'>
+                    <div className='bg-[#D9D9D9] rounded-full mb-3'>
+                        <img src={friend1} alt="Friend" />
+                    </div>
+                    <h2 className='text-[20px] font-semibold mb-2 text-[#1F2937]'>David Kim</h2>
+                    <p className='text-[#64748B] text-[12px] mb-2'>62d ago</p>
+                    <div className="badge badge-success bg-[#CBFADB] border-[#CBFADB] text-[#244D3F] rounded-2xl font-medium text-[12px] uppercase py-1.5 px-2 mb-2">Work</div>
+                    <div className="badge badge-success bg-[#EFAD44] border-[#EFAD44] text-base-100 rounded-2xl font-medium text-[12px] capitalize py-1.5 px-2 mb-2">Almost Due</div>
+                    <div className="badge badge-success bg-[#EF4444] border-[#EF4444] text-base-100 rounded-2xl font-medium text-[12px] capitalize py-1.5 px-2 mb-2">Overdue</div>
+                    <div className="badge badge-success bg-[#244D3F] border-[#244D3F] text-base-100 rounded-2xl font-medium text-[12px] capitalize py-1.5 px-2 mb-2">On-Track</div>
+                </div>
+
+                {/* Friend 1 */}
+                <div className='card bg-base-100 shadow text-center rounded-xl p-8 justify-center items-center'>
+                    <div className='bg-[#D9D9D9] rounded-full mb-3'>
+                        <img src={friend1} alt="Friend" />
+                    </div>
+                    <h2 className='text-[20px] font-semibold mb-2 text-[#1F2937]'>David Kim</h2>
+                    <p className='text-[#64748B] text-[12px] mb-2'>62d ago</p>
+                    <div className="badge badge-success bg-[#CBFADB] border-[#CBFADB] text-[#244D3F] rounded-2xl font-medium text-[12px] uppercase py-1.5 px-2 mb-2">Work</div>
+                    <div className="badge badge-success bg-[#EFAD44] border-[#EFAD44] text-base-100 rounded-2xl font-medium text-[12px] capitalize py-1.5 px-2 mb-2">Almost Due</div>
+                    <div className="badge badge-success bg-[#EF4444] border-[#EF4444] text-base-100 rounded-2xl font-medium text-[12px] capitalize py-1.5 px-2 mb-2">Overdue</div>
+                    <div className="badge badge-success bg-[#244D3F] border-[#244D3F] text-base-100 rounded-2xl font-medium text-[12px] capitalize py-1.5 px-2 mb-2">On-Track</div>
+                </div>
+
+                {/* Friend 1 */}
+                <div className='card bg-base-100 shadow text-center rounded-xl p-8 justify-center items-center'>
+                    <div className='bg-[#D9D9D9] rounded-full mb-3'>
+                        <img src={friend1} alt="Friend" />
+                    </div>
+                    <h2 className='text-[20px] font-semibold mb-2 text-[#1F2937]'>David Kim</h2>
+                    <p className='text-[#64748B] text-[12px] mb-2'>62d ago</p>
+                    <div className="badge badge-success bg-[#CBFADB] border-[#CBFADB] text-[#244D3F] rounded-2xl font-medium text-[12px] uppercase py-1.5 px-2 mb-2">Work</div>
+                    <div className="badge badge-success bg-[#EFAD44] border-[#EFAD44] text-base-100 rounded-2xl font-medium text-[12px] capitalize py-1.5 px-2 mb-2">Almost Due</div>
+                    <div className="badge badge-success bg-[#EF4444] border-[#EF4444] text-base-100 rounded-2xl font-medium text-[12px] capitalize py-1.5 px-2 mb-2">Overdue</div>
+                    <div className="badge badge-success bg-[#244D3F] border-[#244D3F] text-base-100 rounded-2xl font-medium text-[12px] capitalize py-1.5 px-2 mb-2">On-Track</div>
+                </div>
+
+                {/* Friend 1 */}
+                <div className='card bg-base-100 shadow text-center rounded-xl p-8 justify-center items-center'>
+                    <div className='bg-[#D9D9D9] rounded-full mb-3'>
+                        <img src={friend1} alt="Friend" />
+                    </div>
+                    <h2 className='text-[20px] font-semibold mb-2 text-[#1F2937]'>David Kim</h2>
+                    <p className='text-[#64748B] text-[12px] mb-2'>62d ago</p>
+                    <div className="badge badge-success bg-[#CBFADB] border-[#CBFADB] text-[#244D3F] rounded-2xl font-medium text-[12px] uppercase py-1.5 px-2 mb-2">Work</div>
+                    <div className="badge badge-success bg-[#EFAD44] border-[#EFAD44] text-base-100 rounded-2xl font-medium text-[12px] capitalize py-1.5 px-2 mb-2">Almost Due</div>
+                    <div className="badge badge-success bg-[#EF4444] border-[#EF4444] text-base-100 rounded-2xl font-medium text-[12px] capitalize py-1.5 px-2 mb-2">Overdue</div>
+                    <div className="badge badge-success bg-[#244D3F] border-[#244D3F] text-base-100 rounded-2xl font-medium text-[12px] capitalize py-1.5 px-2 mb-2">On-Track</div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Friends;
--- keen-keeper/src/components/HomePage/HomePage.jsx
+++ keen-keeper/src/components/HomePage/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hero from './Hero/Hero';
 import Stats from './Stats/Stats';
+import Friends from './Friends/Friends';
 
 const HomePage = () => {
     return (
         <>
             <Hero></Hero>
             <Stats></Stats>
+            <Friends></Friends>
         </>
     );
 };
