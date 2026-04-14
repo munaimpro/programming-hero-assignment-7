48670a1225e9eff8756ca5cb61e3269ac9b8f75e
Munaim Khan <khanmail2599@gmail.com>
2026-04-14 16:25:16 +0600

    feat: not-found page design and friends data collected

--- keen-keeper/src/components/NotFoundPage/NotFoundPage.jsx
+++ keen-keeper/src/components/NotFoundPage/NotFoundPage.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import { Home } from 'lucide-react';
+import { Link } from 'react-router';
 
 const NotFoundPage = () => {
     return (
-        <div>
-            <h1>404 | Not Found</h1>
+        <div className='container max-w-md flex flex-col mx-auto min-h-screen items-center justify-center text-center'>
+            <h1 className='text-8xl font-black text-[#244D3F] mb-5'>404</h1>
+            <h2 className='text-2xl font-bold mb-5'>Page Not Found</h2>
+            <p className='mb-5 text-[#64748B]'>Looks like this friendship link is broken. The page you're looking for doesn't exist or has been moved.</p>
+            <Link to={'/'}>
+                <button className='flex btn btn-lg capitalize text-base-100 bg-[#244D3F] font-bold'>
+                    <Home size={20} />
+                    <span>Back to home</span>
+                </button>
+            </Link>
         </div>
     );
 };
