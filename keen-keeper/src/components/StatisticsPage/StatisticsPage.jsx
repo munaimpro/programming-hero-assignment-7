8f86017ded630ad808cb5f323d52f09f673466ea
Munaim Khan <khanmail2599@gmail.com>
2026-04-14 14:26:34 +0600

    feat: timeline page design

--- keen-keeper/src/components/StatisticsPage/StatisticsPage.jsx
+++ keen-keeper/src/components/StatisticsPage/StatisticsPage.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 
 const StatisticsPage = () => {
     return (
-        <div>
+        <div className='container mx-auto mt-20'>
+            <h2 className='text-[#1F2937] font-semibold text-2xl mb-4'>Timeline</h2>
             
+            <div className=''>
+                <select class="select">
+                    <option value="all" disabled="" selected="">Filter timeline</option>
+                    <option value="text">Text</option>
+                    <option value="call">Call</option><option value="video">Video</option></select>
+            </div>
         </div>
     );
 };
--- keen-keeper/src/components/Timeline/Timeline.jsx
+++ keen-keeper/src/components/Timeline/Timeline.jsx
@@ -1,9 +1,55 @@
 import React from 'react';
+import callIcon from '../../assets/call.png';
+import textIcon from '../../assets/text.png';
+import videoIcon from '../../assets/video.png';
 
 const Timeline = () => {
     return (
-        <div>
-            
+        <div className='container mx-auto mt-20'>
+            <h2 className='text-[#1F2937] font-semibold text-2xl mb-4'>Timeline</h2>
+
+            <div className='my-6'>
+                <select class="select">
+                    <option value="all" disabled="" selected="">Filter timeline</option>
+                    <option value="text">Text</option>
+                    <option value="call">Call</option><option value="video">Video</option></select>
+            </div>
+
+            <div className='shadow-card items-center bg-base-100 flex gap-4 p-3 rounded-xl mb-4'>
+                <div className='w-fit'>
+                    <img src={callIcon} alt="call" />
+                </div>
+                <div className='w-fit'>
+                    <img src={textIcon} alt="call" />
+                </div>
+                <div className='w-fit'>
+                    <img src={videoIcon} alt="call" />
+                </div>
+                <div className='content w-fit'>
+                    <p className='text-[18px] text-[#64748B]'>
+                        <span className='capitalize font-medium text-[20px] text-[#244D3F]'>meetup</span> with Tom Baker
+                    </p>
+                    <p className='text-[18px] text-[#64748B]'>March 29, 2026</p>
+                </div>
+            </div>
+
+            <div className='shadow-card items-center bg-base-100 flex gap-4 p-3 rounded-xl mb-4'>
+                <div className='w-fit'>
+                    <img src={callIcon} alt="call" />
+                </div>
+                <div className='w-fit'>
+                    <img src={textIcon} alt="call" />
+                </div>
+                <div className='w-fit'>
+                    <img src={videoIcon} alt="call" />
+                </div>
+                <div className='content w-fit'>
+                    <p className='text-[18px] text-[#64748B]'>
+                        <span className='capitalize font-medium text-[20px] text-[#244D3F]'>meetup</span> with Tom Baker
+                    </p>
+                    <p className='text-[18px] text-[#64748B]'>March 29, 2026</p>
+                </div>
+            </div>
         </div>
     );
 };
