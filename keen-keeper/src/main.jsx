eb99d19cfc3ce2c674f2ab25aa85fe54cc776bf6
Munaim Khan <khanmail2599@gmail.com>
2026-04-14 15:56:13 +0600

    fix: new not-found page add

--- keen-keeper/src/main.jsx
+++ keen-keeper/src/main.jsx
@@ -5,10 +5,10 @@ import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from "react-router";
 import Root from './components/Root/Root.jsx';
 import Timeline from './components/Timeline/Timeline.jsx';
-import NotFound from './components/NotFound/NotFound.jsx';
 import HomePage from './components/HomePage/HomePage.jsx';
 import StatisticsPage from './components/StatisticsPage/StatisticsPage.jsx';
 import DetailsPage from './components/DetailsPage/DetailsPage.jsx';
+import NotFoundPage from './components/NotFoundPage/NotFoundPage.jsx';
 
 const route = createBrowserRouter([
   {
@@ -33,7 +33,7 @@ const route = createBrowserRouter([
         },
         {
           'path': '*',
-          'Component': NotFound
+          'Component': NotFoundPage
         }
       ]
     }
